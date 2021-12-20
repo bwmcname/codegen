89745d1ae5cd2c895766d9fbae3647b9df3cc936
Brian <bwmcname@gmail.com>
2021-12-19 20:35:46 -0500

    remove empty codegen_parse_base.cpp

--- src/codegen_unity.cpp
+++ src/codegen_unity.cpp
@@ -2,7 +2,6 @@
 #include "codegen.cpp"
 #include "codegen_inspect_data.cpp"
 #include "codegen_lex_base.cpp"
-#include "codegen_parse_base.cpp"
 #include "codegen_lex_inspect.cpp"
 #include "codegen_parse_inspect.cpp"
 
